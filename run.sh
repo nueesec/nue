6bf76f341fdbc53e2ea51c529ed6019245a93a20
rsqfai <35722671+nueesec@users.noreply.github.com>
2020-07-05 02:09:22 +0700

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+ulang='y'
+while [ $ulang = 'y'];
+do
+    echo 'WELCOME TO PANEL NUEE NETWORK'
+    echo '[1] Jalankan localhost '
+    echo '[2] Jalankan Bot Telegram '
+    echo '[3] Creat Voucher '
+    echo '[4] Lihat Daftar User '
+    echo '[5] Lihat User Aktif '
+    echo -n 'Get: '
+    read get;
+if [ $get = '1'] || [ $get = '01'];
+then
+    cd storage
+    cd shared
+    cd cli
+    php -S localhost:8080
+elif [ $get = '2'] || [ $get = '02'];
+then
+    cd storage
+    cd shared
+    cd cli
+    cd Saldo
+    php Core_Saldo_Nonsaldo.php
+elif [ $get = '3'] || [ $get = '03'];
+then
+    cd storage
+    cd shared
+    cd cli
+    cd hotspot
+    php add_profile.php
+elif [ $get = '4'] || [ $get = '04'];
+then
+    cd storage
+    cd shared
+    cd cli
+    cd hotspot
+    php user.php
+elif [ $get = '5'] || [ $get = '05'];
+then
+    cd storage
+    cd shared
+    cd cli
+    cd hotspot
+    php user_active.php
+else
+    echo 'ERROR: Wrong input..!'
+    slep 1
+    echo $ulang
+fi
+done
